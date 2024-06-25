87f79850474233819186a6fff5fcf359766a5ef6
Ubuntu <ubuntu@ip-172-31-14-23.us-east-2.compute.internal>
2024-06-25 13:18:00 +0000

    refactor

--- app.py
+++ app.py
@@ -14,4 +14,4 @@ def search_regex(pattern: str, url: str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8008)
